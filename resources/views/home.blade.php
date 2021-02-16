138c0751080c41ea65b8379c771da40a8c5150fc
morenoide <jandromoreno3@gmail.com>
2021-02-16 19:47:19 +0100

    6. Home
    Crear la pagina home y subir la foto que sale en ella

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+@extends('plantilla')
+@section('titulo')
+    home
+@endsection
+@section('contenido')
+    <div class="container">
+        <div class="row">
+            <div class="col-8 text-center m-auto">
+                <h1>Bienvenida</h1>
+                <p>El señor y la señora Dursley, que vivían en el número 4 de Privet Drive,
+                    estaban orgullosos de decir que eran muy normales, afortunadamente. Eran las
+                    últimas personas que se esperaría encontrar relacionadas con algo extraño o
+                    misterioso, porque no estaban para tales tonterías.
+                    El señor Dursley era el director de una empresa llamada Grunnings, que
+                    fabricaba taladros. Era un hombre corpulento y rollizo, casi sin cuello, aunque
+                    con un bigote inmenso. La señora Dursley era delgada, rubia y tenía un cuello
+                    casi el doble de largo de lo habitual, lo que le resultaba muy útil, ya que pasaba
+                    la mayor parte del tiempo estirándolo por encima de la valla de los jardines
+                    para espiar a sus vecinos. Los Dursley tenían un hijo pequeño llamado Dudley,
+                    y para ellos no había un niño mejor que él.
+                    Los Dursley tenían todo lo que querían, pero también tenían un secreto, y
+                    su mayor temor era que lo descubriesen: no habrían soportado que se supiera
+                    lo de los Potter.
+                    La señora Potter era hermana de la señora Dursley, pero no se veían
+                    desde hacía años; tanto era así que la señora Dursley fingía que no tenía
+                    hermana, porque su hermana y su marido, un completo inútil, eran lo más
+                    opuesto a los Dursley que se pudiera imaginar. Los Dursley se estremecían al
+                    pensar qué dirían los vecinos si los Potter apareciesen por la acera. Sabían
+                    que los Potter también tenían un hijo pequeño, pero nunca lo habían visto. El
+                    niño era otra buena razón para mantener alejados a los Potter: no querían que
+                    Dudley se juntara con un niño como aquél.
+                    Nuestra historia comienza cuando el señor y la señora Dursley se
+                    despertaron un martes, con un cielo cubierto de nubes grises que amenazaban
+                    tormenta. Pero nada había en aquel nublado cielo que sugiriera los
+                    acontecimientos extraños y misteriosos que poco después tendrían lugar en
+                    toda la región. El señor Dursley canturreaba mientras se ponía su corbata más
+                    sosa para ir al trabajo, y la señora Dursley parloteaba alegremente mientras
+                    instalaba al ruidoso Dudley en la silla alta.
+                    Ninguno vio la gran lechuza parda que pasaba volando por la ventana.
+                    A las ocho y media, el señor Dursley cogió su maletín, besó a la señora
+                    Dursley en la mejilla y trató de despedirse de Dudley con un beso, aunque no
+                    pudo, ya que el niño tenía un berrinche y estaba arrojando los cereales contra
+                    las paredes. «Tunante», dijo entre dientes el señor Dursley mientras salía de la
+                    casa. Se metió en su coche y se alejó del número 4.</p>
+                    <img src="fotos/foto1.jpg" class="img-fluid">
+            </div>
+        </div>
+    </div>
+@endsection
